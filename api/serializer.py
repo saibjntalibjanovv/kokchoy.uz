502df43380df44d4e1e4c9e18d3fa6e6ef9cb549
saibjntalibdjanov <talibdjanov.04@gmail.com>
2025-11-08 22:04:16 +0500

    api/serializer and backend/settings.py

--- api/serializer.py
+++ api/serializer.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product , Message
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = '__all__'
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
         fields = '__all__'
