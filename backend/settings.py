e7f6bb1eb285978a52a2e46237d437b8a1e70f1b
saibjntalibdjanov <talibdjanov.04@gmail.com>
2025-11-08 23:45:55 +0500

    Settings.py - allowing hosts section has been changed

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-s3u#vpgn$-_c)m9hp40q(^4-wciv&y^t!9bp_kv^vebtqyax^d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'kokchoy-uz.onrender.com', '127.0.0.1', 'localhost' ]
 
 
 # Application definition
